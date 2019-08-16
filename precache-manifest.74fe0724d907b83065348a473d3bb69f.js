c4c090f917d6340137843f6bb7ed5a203236e930
atilganf <atilganyf@gmail.com>
2019-08-16 17:23:41 +0300

    Updates

---
+++ precache-manifest.74fe0724d907b83065348a473d3bb69f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "161cddb42a9ccb25fb9f78d036e7ab17",
+    "url": "/reactToDoApp/index.html"
+  },
+  {
+    "revision": "7ee49cbfd6bc0dc5b2fc",
+    "url": "/reactToDoApp/static/css/main.9e06188a.chunk.css"
+  },
+  {
+    "revision": "dac9830825a6a3e14734",
+    "url": "/reactToDoApp/static/js/2.13ab6a6d.chunk.js"
+  },
+  {
+    "revision": "7ee49cbfd6bc0dc5b2fc",
+    "url": "/reactToDoApp/static/js/main.fdea0c2b.chunk.js"
+  },
+  {
+    "revision": "8d06fb78bf51a6e89432",
+    "url": "/reactToDoApp/static/js/runtime~main.34fc8be4.js"
+  }
+]);
